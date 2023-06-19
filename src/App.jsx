63a3e5319f19ce4011dd598426631053be708854
Ramyroh <levyramiiro@gmail.com>
2023-06-19 17:09:15 -0300

    Função colocadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
-
 import React, { useState } from 'react'
 import './App.css'
-import Lista from './Lista/index'
+import Lista from './Lista'
+import { CiSearch } from 'react-icons/ci'
 
 function App() {
 
   const [typed, setTyped] = useState('')
+  const [registro, setRegistro] = useState([])
 
-  function HandleClick() {
-    
+  function handleClick() {
+
+    if (typed != 0) {
+      const newList = {
+        id: registro.length + 1,
+        nome: typed,
+        time: new Date().toLocaleDateString('pt-br', {
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit'
+        })
+      }
+      setRegistro([...registro, newList])
+
+    } else {
+      alert('Invalido')
+    }
   }
-  
+
+  function Delete(id) {
+    let listaFiltrada = registro.filter(item => item.id != id)
+    setRegistro(listaFiltrada)
+  }
   return (
     <div className='containerPrimary'>
       <input className='input' type="text" onChange={e => {
         setTyped(e.target.value)
-        console.log(typed);
-        }}/>
+      }} />
 
-
-      <button className='btnSub' onClick={HandleClick}>
+      <button className='btnSub' onClick={handleClick}>
         Adicionar
       </button>
-      
-      <Lista />
+      <div className='containerSearch'>
+        <input type="text" className='seach' placeholder='pesquisar por nome' />
+        {/* <CiSearch /> */}
+      </div>
+
+      <div className='containerListSeach'>
+        {
+          registro.map(item => <Lista key={item.id} name={item.nome} time={item.time} deletar={Delete} id={item.id} />)
+        }
+      </div>
     </div>
   )
 }
--- src/Lista/index.jsx
+++ src/Lista/index.jsx
@@ -1,7 +1,15 @@
 import './style.css'
+import {FiTrash} from 'react-icons/fi'
 
-export default function Lista() {
-    <div className='containerLista'>
-        <p>nome</p>
-    </div>
+export default function Lista({ name, time, id, deletar}) {
+    return (
+        <div className='containerLista'>
+            <div>
+                <p>{name}</p>
+                <p>{time}</p>
+            </div>
+            <FiTrash className='icon' onClick={() =>  deletar(id)}/>
+        </div>
+    )
+
 }
